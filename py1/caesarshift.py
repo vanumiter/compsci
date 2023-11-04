5d8449be3cc5c396cb86b6aa40bb8d4791c46d92
vanumiter <ivanlv7@outlook.com>
2023-11-04 15:35:32 +0000

    removed redundant list

--- py1/caesarshift.py
+++ py1/caesarshift.py
@@ -1,14 +1,9 @@
 text = str.lower(input("Enter text: "))
 key = int(input("Enter how many letters the text should be shifted: "))
 
-chars = []
-for i in range(len(text)):
-    chars.append(text[i])
-# Adds each letter of text to table "char"
-
 ascii = []
-for i in range(len(chars)):
-    ascii.append(ord(chars[i])+key)
+for i in range(len(text)):
+    ascii.append(ord(text[i])+key)
 # Converts each letter of the table "char" to an ASCII decimal + the key and adds them to table "ascii"
 
 encrypt = ""
