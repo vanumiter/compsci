3b854c9385e007a055c55aae5c69c6b65f7c7c0c
vanumiter <ivanlv7@outlook.com>
2023-11-04 15:36:16 +0000

    sigh

--- py1/caesarshift.py
+++ py1/caesarshift.py
@@ -4,7 +4,7 @@ key = int(input("Enter how many letters the text should be shifted: "))
 ascii = []
 for i in range(len(text)):
     ascii.append(ord(text[i])+key)
-# Converts each letter of the table "char" to an ASCII decimal + the key and adds them to table "ascii"
+# Converts each letter of the input to an ASCII decimal + the key and adds them to table "ascii"
 
 encrypt = ""
 for i in range(len(ascii)):
